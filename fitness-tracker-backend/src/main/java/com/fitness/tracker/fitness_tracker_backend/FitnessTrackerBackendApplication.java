5937d9d4e3bd632d03946020a322b1e65c1957e7
piyushzade00 <piyushzade00@gmail.com>
2025-11-02 19:42:29 +0530

    chore: init spring boot project with dependencies and base structure

---
+++ fitness-tracker-backend/src/main/java/com/fitness/tracker/fitness_tracker_backend/FitnessTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.tracker.fitness_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessTrackerBackendApplication.class, args);
+	}
+
+}
